fb552b9744953bfe3c3f56cea834cfb4bb7096d1
DanyloKozak <danilo.kozak2003@gmail.com>
2024-06-21 19:02:16 +0300

    Added archetype

---
+++ archetype/src/main/resources/archetype-resources/module-math/src/main/java/math/MathOperations.java
@@ -0,0 +1,16 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package math;
+
+public class MathOperations {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+}
---
+++ archetype/src/main/resources/archetype-resources/module-ui/src/main/java/ui/CalculatorUI.java
@@ -0,0 +1,92 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ui;
+
+import net.miginfocom.swing.MigLayout;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.NumberFormatter;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.text.NumberFormat;
+
+public class CalculatorUI {
+
+    public JFrame window;
+    public JPanel panel;
+    public JTextField firstNumber;
+    public JTextField secondNumber;
+    public JComboBox<String> operation;
+    public JTextField result;
+
+    public CalculatorUI() {
+        window = new JFrame("Calculator");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(400,100);
+        window.setLocationRelativeTo(null);
+        setBackground();
+        window.setLayout(new FlowLayout());
+        panel = new JPanel();
+        panel.setLayout(new MigLayout("", "[grow]", "[grow]"));
+
+        NumberFormatter formatter = new NumberFormatter(NumberFormat.getInstance());
+        formatter.setValueClass(Integer.class);
+        formatter.setMinimum(0);
+        formatter.setMaximum(Integer.MAX_VALUE);
+        formatter.setAllowsInvalid(false);
+        formatter.setCommitsOnValidEdit(true);
+        firstNumber = new JTextField(10);
+        secondNumber = new JTextField(10);
+        operation = new JComboBox<>(new String[]{"+", "*"});
+        result = new JTextField();
+        result.setColumns(10);
+        result.setEditable(false);
+
+        panel.add(firstNumber);
+        panel.add(operation);
+        panel.add(secondNumber);
+        panel.add(result);
+        panel.setBackground(null);
+        panel.setOpaque(false);
+        window.add(panel);
+        window.setVisible(true);
+
+        firstNumber.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+
+            }
+        });
+    }
+
+    private void setBackground() {
+        BufferedImage background = null;
+        try {
+            background = ImageIO.read(new File("${artifactId}/src/main/resources/background.jpg"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        JLabel backgroundLabel = new JLabel(new ImageIcon(background.getScaledInstance(400,100, Image.SCALE_SMOOTH)));
+        backgroundLabel.setSize(400,100);
+        backgroundLabel.setLocation(0, 0);
+        window.setLayout(new BorderLayout());
+        window.setContentPane(backgroundLabel);
+    }
+
+}
